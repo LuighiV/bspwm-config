0f83d7a6248fd166c2d81f20946355055ffa6bf8
Luighi <luighiavz@gmail.com>
2021-08-04 21:06:10 -0500

    Add configurations for two monitors

---
+++ polybar/launch_two.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bar1 
+echo "---" | tee -a /tmp/polybar_two_1.log 
+polybar mainbar 2>&1 | tee -a /tmp/polybar_two_1.log & disown
+
+# Launch bar2
+echo "---" | tee -a /tmp/polybar_two_2.log 
+polybar external 2>&1 | tee -a /tmp/polybar_two_2.log & disown
+
+echo "Bars launched..."
--- polybarconfig.sh
+++ polybarconfig.sh
@@ -7,3 +7,4 @@ mkdir -p ~/.config/polybar
 
 ln -s $PWD/polybar/config ~/.config/polybar/config
 ln -s $PWD/polybar/launch.sh ~/.config/polybar/launch.sh
+ln -s $PWD/polybar/launch_two.sh ~/.config/polybar/launch_two.sh
