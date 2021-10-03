695d21387bc960cff14fb82a5e7fe75e7f56e284
Luighi <luighiavz@gmail.com>
2021-10-03 10:23:36 -0500

    Add script to add config for polywins

--- polybarconfig.sh
+++ polybarconfig.sh
@@ -8,3 +8,8 @@ mkdir -p ~/.config/polybar
 ln -s $PWD/polybar/config ~/.config/polybar/config
 ln -s $PWD/polybar/launch.sh ~/.config/polybar/launch.sh
 ln -s $PWD/polybar/launch_two.sh ~/.config/polybar/launch_two.sh
+
+POLYWINS_FILE="${PWD}/polybar/polywins/polywins.sh"
+if [[ -f $POLYWINS_FILE ]]; then
+  ln -s $POLYWINS_FILE ~/.config/polybar/polywins.sh
+fi
